86e2778b88314bf2da57f5107474c59c9dab2831
Volodymyr Stupin <vovastupin96@gmail.com>
2022-09-14 22:19:38 +0200

    hw5

--- src/ua/stupin/hw5/Task2.java
+++ src/ua/stupin/hw5/Task2.java
@@ -1,5 +1,29 @@
 package ua.stupin.ua.stupin.hw5;
 
-public class Task2 {
+import java.util.Arrays;
 
+//Проверить, заданный массив на упорядоченность по невозрастанию,
+// т. е. определить, верно ли, что каждый его элемент, начиная со второго, не больше предыдущего.
+//        *передать массив в метод, получить boolean
+public class Task2 {
+    public static void main(String[] args) {
+        int[] noneOrderedArray = {5, 10, 3, 2, 1, 1};
+        int[] copyNoneOrderedArray = new int[noneOrderedArray.length];
+        System.arraycopy(noneOrderedArray, 0, copyNoneOrderedArray, 0, copyNoneOrderedArray.length);
+        System.out.println(Arrays.toString(noneOrderedArray));
+        System.out.println("Array is ordered by increase: " + checkArrayByNonIncrease(copyNoneOrderedArray));
+    }
+
+    public static boolean checkArrayByNonIncrease(int[] copyNoneOrderedArray) {
+        boolean ArrayIsOrderedByIncrease = false;
+        for(int i = 1; i < copyNoneOrderedArray.length; i++){
+            if (copyNoneOrderedArray[i-1] < copyNoneOrderedArray[i]){
+                ArrayIsOrderedByIncrease = false;
+                break;
+            } else {
+                ArrayIsOrderedByIncrease = true;
+            }
+        }
+    return ArrayIsOrderedByIncrease;
+    }
 }
