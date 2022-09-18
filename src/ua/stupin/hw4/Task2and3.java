aafcf8d0f31475af1a52f214b6259bc407d92bbc
Volodymyr Stupin <vovastupin96@gmail.com>
2022-09-18 13:31:12 +0200

    hw4 change

--- src/ua/stupin/hw4/Task2and3.java
+++ src/ua/stupin/hw4/Task2and3.java
@@ -2,17 +2,23 @@ package ua.stupin.ua.stupin.hw4;
 
 public class Task2and3 {
     public static void main(String[] args) {
-        int[] arrayThousand = new int[1000];
+        int[] arrayThousand = new int[100];
         int primeNumber = 0;
         int compositeNumber = 0;
-        int[] compositeAndPrimeNumber = compositeAndPrimeNumber(arrayThousand, primeNumber, compositeNumber);
+        int notCompositeAndNotPrimeNumber = 0;
+        int[] compositeAndPrimeNumber = compositeAndPrimeNumber(arrayThousand, primeNumber,
+                compositeNumber, notCompositeAndNotPrimeNumber);
         System.out.println("Prime number in array: " + compositeAndPrimeNumber[0]);
         System.out.println("Composite number in array: " + compositeAndPrimeNumber[1]);
+        System.out.println("Not a composite and not a prime number in array: " + compositeAndPrimeNumber[2]);
     }
-    private static int[] compositeAndPrimeNumber (int[] arrayThousand, int primeNumber,int compositeNumber){
+    private static int[] compositeAndPrimeNumber (int[] arrayThousand, int primeNumber,
+                                                  int compositeNumber, int notCompositeAndNotPrimeNumber){
         for (int i = 0; i < arrayThousand.length; i++) {
-            arrayThousand[i] = (2 + (int) (Math.random() * 100));
-            if(arrayThousand[i] == 2 || arrayThousand[i] == 3 ||
+            arrayThousand[i] = ((int) (Math.random() * 10));
+            if (arrayThousand[i] == 0 || arrayThousand[i] == 1) {
+                notCompositeAndNotPrimeNumber += 1;
+            } else if (arrayThousand[i] == 2 || arrayThousand[i] == 3 ||
                     arrayThousand[i] == 5 || arrayThousand[i] == 7) {
                 primeNumber += 1;
             } else if (arrayThousand[i] % 2 == 0 || arrayThousand[i] % 3 == 0 ||
@@ -23,6 +29,6 @@ public class Task2and3 {
             }
             System.out.println(arrayThousand[i]);
         }
-        return new int[] {primeNumber, compositeNumber};
+        return new int[] {primeNumber, compositeNumber, notCompositeAndNotPrimeNumber};
     }
 }
