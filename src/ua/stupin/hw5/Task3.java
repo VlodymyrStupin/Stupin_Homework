8053329be6ba2972228de73d93292ad2e752f0ef
Volodymyr Stupin <vovastupin96@gmail.com>
2022-09-19 22:52:41 +0200

    hw5 change

--- src/ua/stupin/hw5/Task3.java
+++ src/ua/stupin/hw5/Task3.java
@@ -9,44 +9,47 @@ import java.util.Arrays;
 
 public class Task3 {
     public static void main(String[] args) {
-        int[][] twoDimensionalArray = new int [2][2];
-        int[][] copyTwoDimensionalArray = new int[twoDimensionalArray.length][twoDimensionalArray.length];
-        System.arraycopy(twoDimensionalArray, 0, copyTwoDimensionalArray, 0, copyTwoDimensionalArray.length);
-        twoDimensionalArray = filedTwoDimensionalArray(copyTwoDimensionalArray);
-        System.out.println("");
+        int[][] twoDimensionalArray = new int[4][4];
+        System.out.println("Filled array");
+        fillTwoDimensionalArray(twoDimensionalArray);
+        printFiledTwoDimensionalArray(twoDimensionalArray);
+        System.out.println("Rotated array");
         rotateArray(twoDimensionalArray);
+        System.out.println("Filled array after rotate");
+        System.out.println(Arrays.deepToString(twoDimensionalArray));
     }
-
-
-    public static int[][] filedTwoDimensionalArray(int[][] emptyTwoDimensionalArray){
+    public static int[][] fillTwoDimensionalArray(int[][] emptyTwoDimensionalArray) {
         for (int i = 0; i < emptyTwoDimensionalArray.length; i++) {
             for (int j = 0; j < emptyTwoDimensionalArray[i].length; j++) {
-                emptyTwoDimensionalArray[i][j] = (int)(Math.random()*10);
+                emptyTwoDimensionalArray[i][j] = (int) (Math.random() * 10);
             }
         }
-        for (int i = 0; i < emptyTwoDimensionalArray.length; i++){
-            for (int j = 0; j < emptyTwoDimensionalArray.length; j++){
+        return emptyTwoDimensionalArray;
+    }
+    public static int[][] printFiledTwoDimensionalArray(int[][] emptyTwoDimensionalArray) {
+        for (int i = 0; i < emptyTwoDimensionalArray.length; i++) {
+            for (int j = 0; j < emptyTwoDimensionalArray.length; j++) {
                 System.out.print(emptyTwoDimensionalArray[i][j] + " ");
             }
             System.out.println();
         }
-        return  emptyTwoDimensionalArray;
+        return emptyTwoDimensionalArray;
     }
-
-    public static int[][] rotateArray (int[][] twoDimensionalArray){
-        int [][] newTwoDimensionalArray = new int[twoDimensionalArray[0].length][twoDimensionalArray.length];
-        for (int i = 0; i < twoDimensionalArray.length; i++) {
-            for (int j = 0; j < twoDimensionalArray[i].length; j++) {
-                newTwoDimensionalArray[j][i] = twoDimensionalArray[i][j];
+    public static int[][] rotateArray(int[][] twoDimensionalArray) {
+        int[][] copyTwoDimensionalArray = new int[twoDimensionalArray.length][twoDimensionalArray.length];
+        System.arraycopy(twoDimensionalArray, 0, copyTwoDimensionalArray, 0, copyTwoDimensionalArray.length);
+        int[][] newTwoDimensionalArray = new int[copyTwoDimensionalArray[0].length][copyTwoDimensionalArray.length];
+        for (int i = 0; i < copyTwoDimensionalArray.length; i++) {
+            for (int j = 0; j < copyTwoDimensionalArray[i].length; j++) {
+                newTwoDimensionalArray[j][i] = copyTwoDimensionalArray[i][j];
             }
         }
-        for (int i = 0; i < newTwoDimensionalArray.length; i++){
-            for (int j = 0; j < newTwoDimensionalArray.length; j++){
+        for (int i = 0; i < newTwoDimensionalArray.length; i++) {
+            for (int j = 0; j < newTwoDimensionalArray.length; j++) {
                 System.out.print(newTwoDimensionalArray[i][j] + " ");
             }
             System.out.println();
         }
-        return  newTwoDimensionalArray;
+        return newTwoDimensionalArray;
     }
-
 }
