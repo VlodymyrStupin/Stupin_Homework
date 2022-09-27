70f942ca184e450be237cc9659b980be9a162a68
Volodymyr Stupin <vovastupin96@gmail.com>
2022-09-27 20:47:34 +0200

    hw7 - changes

--- src/ua/stupin/hw7/Vector.java
+++ src/ua/stupin/hw7/Vector.java
@@ -1,15 +1,13 @@
 package ua.stupin.ua.stupin.hw7;
 
-import java.util.Arrays;
+public class Vector {
 
-public class Task2 {
-    static class Vectors {
         double x;
         double y;
         double z;
 
 
-        Vectors(double x, double y, double z) {
+        Vector(double x, double y, double z) {
             this.x = x;
             this.y = y;
             this.z = z;
@@ -20,29 +18,29 @@ public class Task2 {
                     Math.pow(y, 2) + Math.pow(z, 2));
         }
 
-        private Vectors calculateProductOfTwoVectors(Vectors vectors) {
-            return new Vectors(y * vectors.z - z * vectors.y,
+        private Vector calculateProductOfTwoVectors(Vector vectors) {
+            return new Vector(y * vectors.z - z * vectors.y,
                     z * vectors.x - x * vectors.z,
                     x * vectors.y - y * vectors.x);
         }
 
-        private double calculateCosineOfTheAngleBetweenTheVectors(Vectors vectors) {
+        private double calculateCosineOfTheAngleBetweenTheVectors(Vector vectors) {
             double scalarProduct = x * vectors.x + y * vectors.y + z * vectors.z;
             return scalarProduct / (calculateVectorLength() * scalarProduct);
         }
 
-        private Vectors addingOfVectors(Vectors vectors) {
-            return new Vectors(x + vectors.x, y + vectors.y, z + vectors.z);
+        private Vector addingOfVectors(Vector vectors) {
+            return new Vector(x + vectors.x, y + vectors.y, z + vectors.z);
         }
 
-        private Vectors subtractOfVectors(Vectors vectors) {
-            return new Vectors(x - vectors.x, y - vectors.y, z - vectors.z);
+        private Vector subtractOfVectors(Vector vectors) {
+            return new Vector(x - vectors.x, y - vectors.y, z - vectors.z);
         }
 
-        public static Vectors[] returnRandomVectorsFromNumberN(int n) {
-            Vectors[] randomVectors = new Vectors[n];
+        public static Vector[] returnRandomVectorsFromNumberN(int n) {
+            Vector[] randomVectors = new Vector[n];
             for (int i = 0; i < n; i++) {
-                randomVectors[i] = new Vectors((Math.random() * 5) - 5, (Math.random() * 5) - 5, (Math.random() * 5) - 5);
+                randomVectors[i] = new Vector((Math.random() * 5) - 5, (Math.random() * 5) - 5, (Math.random() * 5) - 5);
             }
             return randomVectors;
         }
@@ -54,10 +52,10 @@ public class Task2 {
                     ", z=" + z +
                     '}';
         }
-    }
+
 
     public static void main(String[] args) {
-        Vectors[] randomVectors = Vectors.returnRandomVectorsFromNumberN(3);
+        Vector[] randomVectors = Vector.returnRandomVectorsFromNumberN(3);
         System.out.println(randomVectors[1]);
         System.out.println("Result calculation vector length: " + randomVectors[0].calculateVectorLength());
         System.out.println("Result calculation product of two vectors: " +
