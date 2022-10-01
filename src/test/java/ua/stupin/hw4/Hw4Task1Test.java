419b90f7128a6416d021bc6b5d036ab395146fac
Volodymyr Stupin <vovastupin96@gmail.com>
2022-10-02 01:07:10 +0200

    main

---
+++ src/test/java/ua/stupin/hw4/Hw4Task1Test.java
@@ -0,0 +1,36 @@
+package ua.stupin.hw4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Hw4Task1Test {
+    private Task1 task1;
+    @BeforeEach
+    void setUp() {
+        task1 = new Task1();
+    }
+
+    @Test
+    public void testArithmeticAverage1() {
+        // expectation true
+        Assertions.assertEquals(10, task1.getArithmeticAverage(new int[]{10, 10, 10, 10, 10}));
+    }
+
+    @Test
+    public void testArithmeticAverage2() {
+        // expectation false
+        Assertions.assertEquals(10, task1.getArithmeticAverage(new int[]{10, 10, 10, 10, 9}));
+    }
+    @Test
+    public void testGetGeometricMean1() {
+        // expectation true
+        Assertions.assertEquals(5, task1.getGeometricMean(new int[]{5, 5, 5, 5, 5}));
+    }
+
+    @Test
+    public void testGetGeometricMean2() {
+        // expectation false
+        Assertions.assertEquals(10, task1.getGeometricMean(new int[]{10, 10, 10, 10, 9}));
+    }
+}
