e845d47667976f35617e02b1f788bd68508d6204
Volodymyr Stupin <vovastupin96@gmail.com>
2022-10-01 20:48:19 +0200

    hw 10

---
+++ src/main/java/ua/stupin/hw10/Student.java
@@ -0,0 +1,56 @@
+package ua.stupin.hw10;
+
+public class Student {
+    String firstName;
+    String lastName;
+    String group;
+    double averageMark;
+
+    Student(String firstName, String lastName, String group, double averageMark) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.averageMark = averageMark;
+    }
+
+    public String getScholarship() {
+        if (averageMark == 5) {
+            return "Scholarship for " + firstName + " " + lastName + " = 100 UAH";
+        }
+        return "Scholarship for " + firstName + " " + lastName + " = 80 UAH";
+    }
+}
+
+class Aspirant extends Student {
+    String scienceWork;
+
+    Aspirant(String firstName, String lastName, String group, double averageMark, String scienceWork) {
+        super(firstName, lastName, group, averageMark);
+        this.scienceWork = scienceWork;
+    }
+
+    @Override
+    public String getScholarship() {
+        if (averageMark == 5) {
+            return "Scholarship for " + firstName + " " + lastName + " = 200 UAH";
+        }
+        return "Scholarship for " + firstName + " " + lastName + " = 180 UAH";
+    }
+
+    //    г) Создать массив типа Student, содержащий объекты класса Student и Aspirant.
+//    Вызвать метод getScholarship() для каждого элемента массива.
+    public static void main(String[] args) {
+        Student[] students = new Student[5];
+        students[0] = new Student("Bob", "Marley", "A1", 5);
+        students[1] = new Student("John", "Lennon", "A2", 4.38);
+        students[2] = new Student("Bob", "Dylan", "A2", 2.5);
+        students[3] = new Aspirant("Bob", "Dylan", "A2", 2.5, "Bob's science work");
+        students[4] = new Aspirant("Freddie", "Mercury", "A1", 5, "Freddie's science work");
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i].getScholarship());
+        }
+    }
+}
+
+
+
