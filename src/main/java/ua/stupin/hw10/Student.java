861dbcb054e46a36d89b61d4aa1c88b27aded184
Volodymyr Stupin <vovastupin96@gmail.com>
2022-10-12 06:13:01 +0200

    Revert "main change"
    This reverts commit e97c6a0169cbd71a2ce472f722c3e1ab9f8ddba4.

---
+++ src/main/java/ua/stupin/hw10/Student.java
@@ -0,0 +1,35 @@
+package ua.stupin.hw10;
+
+import java.util.Objects;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public String group;
+    public double averageMark;
+
+    public Student(String firstName, String lastName, String group, double averageMark) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.averageMark = averageMark;
+    }
+
+    public int getScholarship() {
+        if (averageMark == 5) {
+            return 100;
+        }
+        return 80;
+    }
+
+    public boolean equals(Student student) {
+        return (this.firstName.equals(student.firstName) &&
+                this.lastName.equals(student.lastName) &&
+                this.group.equals(student.group) &&
+                this.averageMark == student.averageMark);
+    }
+
+    public int hashCode(){
+        return Objects.hash(firstName, lastName, group, averageMark);
+    }
+}
