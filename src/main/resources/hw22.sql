c6c14a424e2806b1542dbfa613502d92d80aadfd
Volodymyr Stupin <vovastupin96@gmail.com>
2022-12-29 19:06:24 +0100

    hw 22

--- src/main/resources/hw22.sql
+++ src/main/resources/hw22.sql
@@ -1,12 +1,29 @@
-SELECT firstName FROM customer WHERE firstName LIKE 'a%';
-SELECT country FROM customer;
-SELECT * FROM customer WHERE sex = "male";
+-- выбрать все записи с firstName начинающимся с А
+SELECT firstName
+FROM customer
+WHERE firstName
+LIKE 'a%';
+--выбрать все записи по стране Ukraine
+SELECT *
+FROM customer
+WHERE customer.country = 'Ukraine'
+--выбрать все записи с полом male
+SELECT *
+FROM customer
+WHERE sex = "male";
+--обновить страну у всех записей с date of birth до 2000 года на USA
 UPDATE customer
 SET country = 'USA'
 WHERE dateOfBirdth < '2000-01-01';
-DELETE FROM customer
+--удалить все записи со страной Russia
+DELETE
+FROM customer
 WHERE country = 'Russia';
-SELECT firstName 
+--выбрать все имена, убрать повторяющиеся (если Alex встречается дважды - в выборке должен быть только 1 Alex)
+SELECT firstName
 FROM customer
 GROUP BY firstName;
-SELECT * FROM customer ORDER BY dateOfBirdth DESC ;
+--выбрать все записи, отсортировать по дате рождения по убыванию
+SELECT *
+FROM customer
+ORDER BY dateOfBirdth DESC ;
